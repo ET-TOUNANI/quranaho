78f25c8d05baf029643430fc73cdf01aec6bce93
adibemohamed <mr.eladib.mohamed@gmail.com>
2021-04-30 00:27:39 +0100

    chore: add surats gallery

---
+++ src/components/Gallery.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="gallery">
+    <div class="grid grid-cols-3 gap-4">
+      <div v-for="i in 114" :key="i" class="bg-green-400 rounded-md p-8">
+        <h1>{{ i }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-
-    <figure class="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
-      <img
-        class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-        src="/sarah-dayan.jpg"
-        alt=""
-        width="384"
-        height="512"
-      />
-      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-          <p class="text-lg font-semibold">
-            “Tailwind CSS is the only framework that I've seen scale on large
-            teams. It’s easy to customize, adapts to any design, and the build
-            size is tiny.”
-          </p>
-        </blockquote>
-        <figcaption class="font-medium">
-          <div class="text-cyan-600">
-            Sarah Dayan
-          </div>
-          <div class="text-gray-500">
-            Staff Engineer, Algolia
-          </div>
-        </figcaption>
-      </div>
-    </figure>
+    <div class="container mx-auto"><Gallery /></div>
   </div>
 </template>
 
 <script lang="ts">
+import Gallery from "@/components/Gallery.vue";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({
-  components: {}
+  components: {
+    Gallery
+  }
 })
 export default class Home extends Vue {}
 </script>

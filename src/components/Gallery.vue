f18b94f88f94d99353737d289dee79b5c42c0dea
adibemohamed <mr.eladib.mohamed@gmail.com>
2021-04-30 01:01:25 +0100

    chore: setup quran api with axios

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,15 +1,52 @@
 <template>
   <div class="gallery">
     <div class="grid grid-cols-3 gap-4">
-      <div v-for="i in 114" :key="i" class="bg-green-400 rounded-md p-8">
-        <h1>{{ i }}</h1>
+      <div
+        v-for="chapter in chapters"
+        :key="chapter.id"
+        class="bg-green-400 rounded-md p-8"
+      >
+        <h1>{{ JSON.stringify(chapter.name_arabic) }}</h1>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  name: "Gallery",
+  data() {
+    return {
+      error: {
+        type: String,
+        default: ""
+      },
+      chapters: {}
+    };
+  },
+  methods: {
+    async fetchChapters() {
+      try {
+        await axios
+          .get("https://api.quran.com/api/v4/chapters?language=en")
+          .then(response => {
+            this.chapters = response.data.chapters;
+            console.log(response.data.chapters[0]);
+          })
+          .catch(error => {
+            this.error = error;
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  mounted() {
+    console.log("Mounted");
+    this.fetchChapters();
+  }
+};
 </script>
 
 <style></style>

135e61521fdc9357955c45d8d6bb075d36ae53a7
adibemohamed <mr.eladib.mohamed@gmail.com>
2021-05-01 23:31:29 +0100

    style: migrate search icon to new componentn

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -24,44 +24,7 @@
               class="absolute inset-y-0 left-0 py-3  pl-3 flex items-center "
             >
               <span class="text-gray-500 sm:text-sm cursor-pointer h-full ">
-                <svg
-                  class="h-full m-0"
-                  version="1.1"
-                  id="Capa_1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 511.999 511.999"
-                  style="enable-background:new 0 0 511.999 511.999;"
-                  xml:space="preserve"
-                >
-                  <g>
-                    <g>
-                      <path
-                        d="M508.874,478.708L360.142,329.976c28.21-34.827,45.191-79.103,45.191-127.309c0-111.75-90.917-202.667-202.667-202.667
-                          S0,90.917,0,202.667s90.917,202.667,202.667,202.667c48.206,0,92.482-16.982,127.309-45.191l148.732,148.732
-                          c4.167,4.165,10.919,4.165,15.086,0l15.081-15.082C513.04,489.627,513.04,482.873,508.874,478.708z M202.667,362.667
-                          c-88.229,0-160-71.771-160-160s71.771-160,160-160s160,71.771,160,160S290.896,362.667,202.667,362.667z"
-                      />
-                    </g>
-                  </g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                  <g></g>
-                </svg>
+                <search-icon />
               </span>
             </div>
             <input
@@ -82,6 +45,7 @@
 
 <script>
 import SearchDropdown from "@/components/SearchDropdown.vue";
+import SearchIcon from "@/components/SearchIcon.vue";
 export default {
   data() {
     return {
@@ -91,7 +55,8 @@ export default {
     };
   },
   components: {
-    SearchDropdown
+    SearchDropdown,
+    SearchIcon
   }
 };
 </script>

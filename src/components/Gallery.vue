b8dbd78de1edb2027e406d4946cc6244562887f0
adibemohamed <mr.eladib.mohamed@gmail.com>
2022-03-02 18:40:43 +0100

    refactor: updates

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -4,11 +4,7 @@
       v-if="!loading"
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 sm:gap-3 md:gap-3 mx-2 sm:mx-0"
     >
-      <chapter-card
-        v-for="chapter in chapters"
-        :key="chapter.id"
-        :chapter="chapter"
-      />
+      <chapter-card v-for="chapter in chapters" :key="chapter.id" :chapter="chapter" />
     </div>
 
     <Loading v-else />
@@ -16,43 +12,49 @@
 </template>
 
 <script lang="ts">
-import axios from "axios";
-import ChapterCard from "./partials/ChapterCard.vue";
-import { defineComponent } from "vue";
-import Loading from "@/components/Loading.vue";
+  import axios from 'axios'
+  import ChapterCard from './partials/ChapterCard.vue'
+  import { defineComponent } from 'vue'
+  import Loading from '@/components/Loading.vue'
 
-export default defineComponent({
-  name: "Gallery",
-  components: {
-    ChapterCard,
-    Loading,
-  },
-  data() {
-    return {
-      loading: true,
-      error: "",
-      chapters: {},
-    };
-  },
-  methods: {
-    async fetchChapters() {
-      await axios
-        .get("https://api.quran.com/api/v4/chapters?language=en")
-        .then((response) => {
-          this.chapters = response.data.chapters;
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-        .finally(() => {
-          this.loading = false;
-        });
+  interface Chapter {
+    id: number
+    name: string
+    verses: number
+  }
+
+  export default defineComponent({
+    name: 'Gallery',
+    components: {
+      ChapterCard,
+      Loading
     },
-  },
-  created() {
-    this.fetchChapters();
-  },
-});
+    data() {
+      return {
+        loading: true,
+        error: '',
+        chapters: [] as Chapter[]
+      }
+    },
+    methods: {
+      async fetchChapters() {
+        await axios
+          .get('https://api.quran.com/api/v4/chapters?language=en')
+          .then((response) => {
+            this.chapters = response.data.chapters
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+          .finally(() => {
+            this.loading = false
+          })
+      }
+    },
+    created() {
+      this.fetchChapters()
+    }
+  })
 </script>
 
 <style></style>

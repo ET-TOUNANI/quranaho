bd8df3656f16034c0f3e09609cabac49fe36649d
adibemohamed <mr.eladib.mohamed@gmail.com>
2022-03-03 02:05:10 +0100

    chore: updates yarn lock

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,47 +3,7 @@
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-4">
       <div class="relative flex items-center justify-between h-16">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <button
-            @click="isHidden = !isHidden"
-            type="button"
-            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-            aria-controls="mobile-menu"
-            aria-expanded="false"
-          >
-            <span class="sr-only">Open main menu</span>
-
-            <svg
-              class="block h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-
-            <svg
-              class="hidden h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
+          <nav-bar-menu-toggler @click="isHidden = !isHidden" :is-hidden="isHidden" />
         </div>
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex-shrink-0 flex items-center">
@@ -57,10 +17,10 @@
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
               <nav-bar-item :to="'/'">
-                <nav-bar-item-label> {{ translatedWords.chapters }} </nav-bar-item-label>
+                <nav-bar-item-label :label="translatedWords.chapters"></nav-bar-item-label>
               </nav-bar-item>
               <nav-bar-item :to="'/about'">
-                <nav-bar-item-label> {{ translatedWords.about }} </nav-bar-item-label>
+                <nav-bar-item-label :label="translatedWords.about"></nav-bar-item-label>
               </nav-bar-item>
             </div>
           </div>
@@ -83,10 +43,10 @@
     <div class="sm:hidden bg-gray-200 dark:bg-gray-700" :class="isHidden ? 'hidden' : ''">
       <div class="px-2 pt-2 pb-3 flex flex-col">
         <nav-bar-item :to="'/'">
-          <nav-bar-item-label> {{ translatedWords.chapters }} </nav-bar-item-label>
+          <nav-bar-item-label :label="translatedWords.chapters"></nav-bar-item-label>
         </nav-bar-item>
         <nav-bar-item :to="'/about'">
-          <nav-bar-item-label> {{ translatedWords.about }} </nav-bar-item-label>
+          <nav-bar-item-label :label="translatedWords.about"></nav-bar-item-label>
         </nav-bar-item>
       </div>
     </div>
@@ -98,6 +58,7 @@
   import { defineComponent } from 'vue'
   import NavBarItem from './NavBarItem.vue'
   import NavBarItemLabel from './NavBarItemLabel.vue'
+  import NavBarMenuToggler from './NavBarMenuToggler.vue'
   export default defineComponent({
     data() {
       return {
@@ -112,7 +73,8 @@
     components: {
       ModeSwitcher,
       NavBarItem,
-      NavBarItemLabel
+      NavBarItemLabel,
+      NavBarMenuToggler
     }
   })
 </script>
